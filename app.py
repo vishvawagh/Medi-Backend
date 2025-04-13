ff549904f3d4fc618cba90bc3a6f71f17a7c4432
Vishvachaitanya Wagh <98593551+vishvawagh@users.noreply.github.com>
2025-04-13 12:01:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import tensorflow as tf
 import numpy as np
 from PIL import Image
 import io
 from dotenv import load_dotenv
-import os
 import json
 import traceback
 import google.generativeai as genai  
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 # Load environment variables
 load_dotenv()
 gemini_api_key = os.getenv("GEMINI_API_KEY")
